936ac309b0ea7d48a71b440b71e7f0c0d537f3d4
Logix5 <logix5@logix5.com>
2018-07-31 00:13:02 +0200

    adlx345 image uploaded

--- blocks/ADXL345.js
+++ blocks/ADXL345.js
@@ -40,7 +40,7 @@ Blockly.Blocks['Init_Accel_ADXL345'] = {
   init: function() {
     this.setColour(220);
 	this.appendDummyInput()
-       // .appendField(new Blockly.FieldImage("images/ds18b20.png",52,38))
+        .appendField(new Blockly.FieldImage("images/ADXL345.png",50,38))
 		.appendField(Blockly.Msg.ADXL345)
 		.appendField(Blockly.Msg.ADXL345_2);
 	this.setInputsInline(true);
@@ -55,7 +55,7 @@ Blockly.Blocks['config_adxl345_range'] = {
   init: function() {
     this.setColour(220);
     this.appendDummyInput()
-	   // .appendField(new Blockly.FieldImage("images/ds18b20.png",52,38))
+	    .appendField(new Blockly.FieldImage("images/ADXL345.png",50,38))
         .appendField(Blockly.Msg.ADXL345_config)
 		.appendField(new Blockly.FieldDropdown([['2g','ADXL345_RANGE_2_G'],['4g','ADXL345_RANGE_4_G'],['8g','ADXL345_RANGE_8_G'],['16g','ADXL345_RANGE_16_G']]), "ADXL345_RANGE");
 	this.setInputsInline(true);
@@ -70,7 +70,7 @@ Blockly.Blocks['order_to_read_adxl345_values'] = {
   init: function() {
     this.setColour(220);
     this.appendDummyInput()
-	   // .appendField(new Blockly.FieldImage("images/ds18b20.png",52,38))
+	    .appendField(new Blockly.FieldImage("images/ADXL345.png",50,38))
         .appendField(Blockly.Msg.ADXL345_read)
 	this.setInputsInline(true);
     this.setPreviousStatement(true, null);
@@ -84,7 +84,7 @@ Blockly.Blocks['adxl345_values'] = {
   init: function() {
     this.setColour(220);
     this.appendDummyInput()
-		//.appendField(new Blockly.FieldImage("images/ds18b20.png",52,38))
+		.appendField(new Blockly.FieldImage("images/ADXL345.png",50,38))
 		.appendField(new Blockly.FieldDropdown([['Ax','0'],['Ay','1'],['Az','2']]), "TypeAccel")
 	    .appendField(Blockly.Msg.ADXL345_values);
     this.setOutput(true, 'Number');
